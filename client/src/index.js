91bc03a47f78f9e47dab8c4d0c764058c0a8656c
Your Name <you@example.com>
2021-04-07 10:43:41 -0500

    first push , imported all the requirements

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route } from 'react-router-dom';
 // Import React Router dom
 import './index.css';
 import App from './App';
 
 // You'll need to wrap <App /> for routing to work
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+      <App />
+    </Router>,
+    document.getElementById("root")
+  );
